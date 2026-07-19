c10db2069d8104129665b7938a0a5f19c59fd0ed
Дмитрий <navrot73@gmail.com>
2026-07-19 13:00:43 +0500

    Expose all fuel records through repository for AI analysis

--- app/src/main/java/com/navrot/aifuelassistant/data/FuelRecordRepository.kt
+++ app/src/main/java/com/navrot/aifuelassistant/data/FuelRecordRepository.kt
@@ -4,6 +4,7 @@ import com.navrot.aifuelassistant.data.database.entity.FuelRecordEntity
 import kotlinx.coroutines.flow.Flow
 
 interface FuelRecordRepository {
+    fun getAll(): Flow<List<FuelRecordEntity>>
     fun getByVehicleId(vehicleId: Long): Flow<List<FuelRecordEntity>>
     suspend fun insert(record: FuelRecordEntity)
     suspend fun update(record: FuelRecordEntity)
