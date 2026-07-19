9c3a8072c570eca58ae02659bae239090daaf198
Дмитрий <navrot73@gmail.com>
2026-07-19 12:06:51 +0500

    Add fuel record repository implementation

---
+++ app/src/main/java/com/navrot/aifuelassistant/data/FuelRecordRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.navrot.aifuelassistant.data
+
+import com.navrot.aifuelassistant.data.database.dao.FuelRecordDao
+import com.navrot.aifuelassistant.data.database.entity.FuelRecordEntity
+import kotlinx.coroutines.flow.Flow
+
+class FuelRecordRepositoryImpl(
+    private val dao: FuelRecordDao
+) : FuelRecordRepository {
+
+    override fun getByVehicleId(vehicleId: Long): Flow<List<FuelRecordEntity>> =
+        dao.getByVehicleId(vehicleId)
+
+    override suspend fun insert(record: FuelRecordEntity) {
+        dao.insert(record)
+    }
+
+    override suspend fun update(record: FuelRecordEntity) {
+        dao.update(record)
+    }
+
+    override suspend fun delete(record: FuelRecordEntity) {
+        dao.delete(record)
+    }
+}
