56ac5130497c93768bb4f76e6c7708bb0c788ddc
Дмитрий <navrot73@gmail.com>
2026-07-19 12:08:50 +0500

    Connect fuel record creation screen to navigation

--- app/src/main/java/com/navrot/aifuelassistant/ui/AppNavigation.kt
+++ app/src/main/java/com/navrot/aifuelassistant/ui/AppNavigation.kt
@@ -7,8 +7,9 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.navrot.aifuelassistant.features.dashboard.DashboardScreen
-import com.navrot.aifuelassistant.ui.vehicles.AddVehicleScreen
+import com.navrot.aifuelassistant.ui.fuel.AddFuelRecordScreen
 import com.navrot.aifuelassistant.ui.fuel.FuelRecordListScreen
+import com.navrot.aifuelassistant.ui.vehicles.AddVehicleScreen
 
 @Composable
 fun AppNavigation() {
@@ -18,19 +19,16 @@ fun AppNavigation() {
         navController = navController,
         startDestination = "dashboard"
     ) {
-        // Главный экран / Дашборд
         composable(route = "dashboard") {
             DashboardScreen()
         }
 
-        // Добавить автомобиль
         composable(route = "add_vehicle") {
             AddVehicleScreen(
                 onNavigateBack = { navController.popBackStack() }
             )
         }
 
-        // Список записей о заправке
         composable(
             route = "fuel_records/{vehicleId}/{vehicleName}",
             arguments = listOf(
@@ -41,12 +39,30 @@ fun AppNavigation() {
             val vehicleId = backStackEntry.arguments?.getLong("vehicleId") ?: 0L
             val vehicleName = backStackEntry.arguments?.getString("vehicleName") ?: ""
 
-            // ИСПРАВЛЕНО: приведены к правильным именам параметры onBack и onAddClick
             FuelRecordListScreen(
                 vehicleId = vehicleId,
                 vehicleName = vehicleName,
                 onBack = { navController.popBackStack() },
-                onAddClick = { /* TODO: navController.navigate("add_fuel_record") */ }
+                onAddClick = {
+                    navController.navigate("add_fuel_record/$vehicleId/$vehicleName")
+                }
+            )
+        }
+
+        composable(
+            route = "add_fuel_record/{vehicleId}/{vehicleName}",
+            arguments = listOf(
+                navArgument("vehicleId") { type = NavType.LongType },
+                navArgument("vehicleName") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val vehicleId = backStackEntry.arguments?.getLong("vehicleId") ?: 0L
+            val vehicleName = backStackEntry.arguments?.getString("vehicleName") ?: ""
+
+            AddFuelRecordScreen(
+                vehicleId = vehicleId,
+                defaultFuelType = "Бензин",
+                onBack = { navController.popBackStack() }
             )
         }
     }
