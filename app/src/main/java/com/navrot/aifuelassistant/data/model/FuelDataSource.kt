a6629ad92caee91a363a22e138df04b4a245d273
Дмитрий <navrot73@gmail.com>
2026-07-30 16:02:47 +0500

    feat: define fuel data source types

---
+++ app/src/main/java/com/navrot/aifuelassistant/data/model/FuelDataSource.kt
@@ -0,0 +1,15 @@
+package com.navrot.aifuelassistant.data.model
+
+/** Source of a fuel station fact. */
+enum class FuelDataSource {
+    GDEBENZ,
+    TWO_GIS,
+    YANDEX,
+    T_BANK,
+    OFFICIAL_STATION,
+    TELEGRAM,
+    VK,
+    USER_REPORT,
+    PHOTO_EVIDENCE,
+    DEMO
+}
