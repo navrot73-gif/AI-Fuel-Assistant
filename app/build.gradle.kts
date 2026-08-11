94f2e832660148b789ecc19a1ad14c893ccb5345
Дмитрий Навроцкий <navrot73@gmail.com>
2026-08-11 22:12:13 +0500

    chore(deps): update Kotlin to 2.0, Compose BOM to 2024.06, Coil to 2.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.hilt)
     id("kotlin-parcelize")
@@ -59,7 +61,6 @@ android {
         compose = true
         buildConfig = true
     }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.8" }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 
     sourceSets {
@@ -88,7 +89,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     // Image loading
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation(libs.coil.compose)
     debugImplementation(libs.androidx.compose.ui.tooling)
 
     // Navigation
